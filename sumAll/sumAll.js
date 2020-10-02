0bf365eba0747fdcde025c62887f91ea2ba076c2
David <david.ngu3@gmail.com>
2020-10-02 15:23:51 +1000

    sumall all tests pass

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let start, end;
 
+    // wrong type check
+    if (typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+        return "ERROR";
+    }
+
+    // negative number check
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    // initialise start and end numbers appropriately
+    if (num2 > num1) {
+        start = num1;
+        end = num2;
+    } 
+    else {
+        start = num2;
+        end = num1;
+    }
+
+    // looping and summing
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 }
 
 module.exports = sumAll
