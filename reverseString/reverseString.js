bef9dafe88707c2b7cb1fc0b6e932019247d25c8
David <david.ngu3@gmail.com>
2020-10-02 15:00:20 +1000

    reversestring all tests pass

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // js has no native string reverse method, so
+    // reverses by converting into array (split) then reversing using array method and stitching back together (join)
+    return string.split('').reverse().join('');
 }
 
 module.exports = reverseString
