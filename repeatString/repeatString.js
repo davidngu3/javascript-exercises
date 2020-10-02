70aea46dec4c19e95e8d30dbd4bf974279168d8e
David <david.ngu3@gmail.com>
2020-10-02 14:57:20 +1000

    repeatstring all passed

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repetitions) {
+    if (repetitions < 0) {
+        return "ERROR";
+    }
 
+    // initialise empty string and +concatenate string specified number of time 
+    let ret = "";
+    for (let i = 0; i < repetitions; i++) {
+        ret += string;
+    }
+
+    return ret;
 }
 
 module.exports = repeatString
