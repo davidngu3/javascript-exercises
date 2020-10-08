af50bcc1c3644fa7a9cc37d7bb87eed96d625e5b
David <david.ngu3@gmail.com>
2020-10-08 12:08:00 +1100

    palindromes 0 errors

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function() {
-
+const palindromes = function(entry) {
+    let plaintext = entry.toLowerCase().replace(/[?!,.\s]/g, ""); 
+    return plaintext == std.split('').reverse().join('');
 }
 
 module.exports = palindromes
