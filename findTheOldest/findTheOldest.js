e820d91ddf7a9bd39071061082cb2987c0fe7c09
David <david.ngu3@gmail.com>
2020-10-10 12:30:59 +1100

    findtheoldest 0 errors reduce

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    let oldestPerson = people.reduce((eldest, person) => {
+        let eldestAge = eldest[1];
 
+        let personDeath = person.yearOfDeath ?? new Date().getFullYear();
+        let personAge = personDeath - person.yearOfBirth;
+        if (personAge > eldestAge) {
+            return [person, personAge];
+        }
+        return eldest;
+    }, [{}, -Infinity]);
+
+    return oldestPerson[0];
 }
 
 module.exports = findTheOldest
